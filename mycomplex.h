c0ab05f5c919929b43bf3bbf8a331f9ee2402de5
Bauman <bauman.e.v@yandex.ru>
2020-05-05 20:21:08 +0500

    partly changed mycomplex.h 2

--- mycomplex.h
+++ mycomplex.h
@@ -10,26 +10,32 @@ class Complex
   Complex( double aRe = 0 , double aIm = 0 );
   Complex( const Complex& );
   ~Complex();
+
   void Set( double aRe, double aIm=0);
   operator double();
   double abs();
   friend istream& operator>>( istream& , Complex& );
+
   friend ostream& operator<< ( ostream&, Complex& );
   Complex operator+ ( const Complex& );
   Complex operator- ( const Complex& );
   Complex operator+ ( const double& );
+
   friend Complex operator+ ( const double& , const Complex& );
   Complex operator- ( const double& );
   friend Complex operator- ( const double& , const Complex& );
   Complex operator* ( const Complex& );
+
   Complex operator* ( const double& );
   friend Complex operator* ( const double&, const Complex& );
   Complex operator/ ( const double& );
   Complex& operator+= (const Complex&);
+
   Complex& operator-= ( const Complex& );
   Complex& operator*= ( const Complex& );
   Complex& operator+= ( const double&);
   Complex& operator-= ( const double&);
+
   Complex& operator*= ( const double& );
   Complex& operator/= ( const double& );
   Complex& operator= ( const Complex& );
